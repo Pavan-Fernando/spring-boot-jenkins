27110872c51ef670a30bd7dd14e0a1fac9f99450
Pavan_Fernando <pavanfernando6@gmail.com>
2024-05-21 12:27:11 +0530

    added logger

--- src/test/java/org/example/springbootjenkins/SpringBootJenkinsApplicationTests.java
+++ src/test/java/org/example/springbootjenkins/SpringBootJenkinsApplicationTests.java
@@ -1,13 +1,18 @@
 package org.example.springbootjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringBootJenkinsApplicationTests {
 
+    private final Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
     @Test
     void contextLoads() {
+        logger.info("test cases running");
     }
 
 }
